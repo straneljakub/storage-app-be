69061d86ffb5e8d6cdb897452113256084601cd8
straneljakub <99950676+straneljakub@users.noreply.github.com>
2023-02-28 18:50:16 +0100

    materials CRUD

--- src/materials/materials/materials.controller.ts
+++ src/materials/materials/materials.controller.ts
@@ -1,13 +1,73 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Res,
+  Body,
+  HttpStatus,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { Material } from '@prisma/client';
+import { CreateMaterialDto } from 'src/dto/material.dto';
+import { ValidationPipe } from '../validation/validation.pipe';
 import { MaterialsService } from './materials.service';
-import { Material } from '.prisma/client';
 
 @Controller('materials')
 export class MaterialsController {
   constructor(private readonly materialsService: MaterialsService) {}
 
   @Get()
-  getAll(): any {
+  async getAll(): Promise<Material[]> {
     return this.materialsService.getAll();
+  }
+
+  @Post('/')
+  async add(
+    @Res() res,
+    @Body(new ValidationPipe()) material: CreateMaterialDto,
+  ) {
+    const newMaterial = this.materialsService.add(material);
+    if (newMaterial) {
+      return res.status(HttpStatus.OK).json({
+        message: 'Material added!',
+        material: await newMaterial,
+      });
+    } else {
+      throw new NotFoundException('Material was not added!');
+    }
+  }
+
+  @Put('/:id')
+  async edit(
+    @Res() res,
+    @Body(new ValidationPipe()) material: CreateMaterialDto,
+    @Param('id', ParseIntPipe) id: number,
+  ) {
+    const edited = this.materialsService.edit(id, material);
+    if (edited) {
+      return res.status(HttpStatus.OK).json({
+        message: 'Material edited!',
+        edited: await edited,
+      });
+    } else {
+      throw new NotFoundException('Material was not edited!');
+    }
+  }
+
+  @Delete('/:id')
+  async delete(@Res() res, @Param('id', ParseIntPipe) id: number) {
+    const deleted = this.materialsService.delete(id);
+    if (deleted) {
+      return res.status(HttpStatus.OK).json({
+        message: 'Material deleted!',
+        deleted: await deleted,
+      });
+    } else {
+      throw new NotFoundException('Material was not deleted!');
+    }
   }
 }
--- src/materials/materials/materials.service.ts
+++ src/materials/materials/materials.service.ts
@@ -1,11 +1,38 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Material, PrismaClient } from '@prisma/client';
+import { CreateMaterialDto } from 'src/dto/material.dto';
 
 @Injectable()
 export class MaterialsService {
   constructor(private readonly prisma: PrismaClient) {}
 
-  getAll(): any {
+  async getAll(): Promise<Material[]> {
     return this.prisma.material.findMany();
+  }
+
+  async add(material: CreateMaterialDto): Promise<Material> {
+    const newMaterial = await this.prisma.material.create({
+      data: material,
+    });
+    return newMaterial;
+  }
+
+  async edit(id: number, material: CreateMaterialDto): Promise<Material> {
+    const edited = this.prisma.material.update({
+      where: {
+        id: id,
+      },
+      data: material,
+    });
+    return edited;
+  }
+
+  async delete(id: number) {
+    const deleted = this.prisma.material.delete({
+      where: {
+        id: id,
+      },
+    });
+    return deleted;
   }
 }
