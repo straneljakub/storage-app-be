7f007176e50dbf4f1340b1146764d1c96587ce82
straneljakub <99950676+straneljakub@users.noreply.github.com>
2023-03-14 11:33:58 +0100

    folders fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MaterialsModule } from './materials/materials/materials.module';
-import { ConditionsModule } from './conditions/conditions/conditions.module';
-import { NotificationsModule } from './notifications/notifications/notifications.module';
+import { MaterialsModule } from './materials/materials.module';
+import { ConditionsModule } from './conditions/conditions.module';
+import { NotificationsModule } from './notifications/notifications.module';
 
 @Module({
   imports: [MaterialsModule, ConditionsModule, NotificationsModule],
